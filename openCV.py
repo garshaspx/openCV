d6a510eb32eddbb87e9f4cddade8ecc569793095
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-07-05 01:06:27 -0700

    ready

--- openCV.py
+++ openCV.py
@@ -2,23 +2,19 @@
 #and save info in exel
 #
 #
-#
-#
-#
+
+
 import cv2
 import os
 import numpy as np
 import pandas as pd
-#import time
 from datetime import datetime
+from openpyxl import Workbook
 
 
-#print(time.localtime())
-
+################# extracting features
 address = "C:\\Users\\garshasp\\pictures"
 sift = cv2.xfeatures2d.SIFT_create()
-
-# extracting features
 for i in os.listdir(address):
     if i[-3:].lower() == "jpg" or i[-3:].lower() == "png":
         image = cv2.imread(address+"\\"+i)
@@ -32,52 +28,33 @@ for i in os.listdir(address):
         print(f"image {i} completed")
 
 
-
-
-# data center func
+############## data center
 excel_address = "C:\\Users\\garshasp\\Documents\\openCV_datacenter.xlsx"
-df = pd.read_excel(excel_address)
-
-def save(img):
+df = pd.DataFrame(columns=["image", "time", "ID"])
+work = Workbook()
+sheet = work.active
+sheet.title = "image_data" #sheetname 
+work.save(filename=excel_address)
+with pd.ExcelWriter(excel_address, engine="openpyxl") as xlsx:
+    df.to_excel(xlsx, sheet_name="image_data")
     
+def save(img):    
     now = datetime.now()
     time = now.strftime("%Y-%m-%d %H:%M:%S")
-    
-    
-    df[len(df.index)] = {"time":time, "image":img}
-    
-    print(df)
+    df.loc[len(df.index)] = [time, img, None]
     with pd.ExcelWriter(excel_address, engine="auto") as excel:
         df.to_excel(excel, sheet_name="image_info")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
-    
-#matching features  
+##############matching features  
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 cam = cv2.VideoCapture(0)
 highest_match = [0, 0]
 
 while True:
-    
     id, image = cam.read()
     BW_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     key, des = sift.detectAndCompute(BW_image, None)
-
-
 
     highest_match[0] = 0
     for i in os.listdir(address+"\\"+"features"):
@@ -94,7 +71,7 @@ while True:
     save(highest_match[1][:-4])
 
     image_hm = cv2.imread(address+"\\"+highest_match[1][:-4]+".jpg")
-    Hori = np.concatenate((image, image_hm), axis=1)
+    matcher = np.concatenate((image, image_hm), axis=1)
     
-    cv2.imshow("two image", Hori)
+    cv2.imshow("two image", matcher)
     cv2.waitKey(1)
