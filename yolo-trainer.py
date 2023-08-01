285cb13c0dce24dba0f2dbf110ad4e46eb504d01
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-08-01 03:40:34 -0700

    update

--- yolo-trainer.py
+++ yolo-trainer.py
@@ -5,7 +5,7 @@ model = YOLO()
 
 print("start")
 
-model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=32)
+model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=30)
 
 
 print("finished")
