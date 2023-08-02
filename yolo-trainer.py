b1c601a2bdffa3b6df1ee6c681fe4b581d9afa54
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-08-02 05:03:23 -0700

    Revert "update"
    This reverts commit 285cb13c0dce24dba0f2dbf110ad4e46eb504d01.

--- yolo-trainer.py
+++ yolo-trainer.py
@@ -5,7 +5,7 @@ model = YOLO()
 
 print("start")
 
-model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=30)
+model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=32)
 
 
 print("finished")
