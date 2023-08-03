2d9cc17e139f766fbdfe301a0964cd17ecd563f9
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-08-03 12:52:42 -0700

    update

--- yolo-detector.py
+++ yolo-detector.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
+
 import cv2
 
-model = YOLO("C:\\Users\\garshasp\\Documents\\GitHub\\openCV\\runs\\detect\\train2\\weights\\best.pt")
+model = YOLO("C:\\Users\\garshasp\\Documents\\best_card_metro_8m.pt")
 cam = cv2.VideoCapture(0)
 
 while True:
     
     id, image = cam.read()
+
     results = model.predict(image)
     result = results[0]
 
@@ -16,15 +18,25 @@ while True:
         cords = box.xyxy[0].tolist()
         cords = [round(x) for x in cords]
         conf = round(box.conf[0].item(), 2)
-    
-        cv2.rectangle(image, (cords[0], cords[1]), (cords[2], cords[3]), 255, 2)
-        cv2.putText(image, class_id, (cords[0], cords[1]-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)   
         
+        if conf >= 0.6:
+            cv2.rectangle(image, (cords[0], cords[1]), (cords[2], cords[3]), 255, 2)
+            cv2.putText(image, class_id, (cords[0], cords[1]-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)   
+            
         print("Object type:", class_id)
         print("Coordinates:", cords)
         print("Probability:", conf)
-
     cv2.imshow("test", image)    
+
     if cv2.waitKey(10) == 27:
         cv2.destroyAllWindows()
         break
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- yolo-trainer.py
+++ yolo-trainer.py
@@ -1,11 +1,11 @@
 
 from ultralytics import YOLO
 
-model = YOLO()
+model = YOLO("C:\\Users\\garshasp\Documents\\yolov8x.pt")
 
 print("start")
 
-model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=32)
+model.train(data="C:\\Users\\garshasp\\Desktop\\data.yaml", epochs=30)
 
 
 print("finished")
