bf323d7590f432c56a4181578576c20f3a804098
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-08-06 09:30:10 -0700

    update sql counter

--- my_sql.py
+++ my_sql.py
@@ -1,14 +1,32 @@
-import mysql.connector
+import sqlite3
+ 
+ 
+# create connection by using object
+# to connect with hotel_data database
+connection = sqlite3.connect('hotel_data.db')
+ 
+# query to create a table named FOOD1
+try:
+    connection.execute(''' CREATE TABLE hotel
+            (FIND INT PRIMARY KEY     NOT NULL,
+            FNAME           TEXT    NOT NULL,
+            COST            INT     NOT NULL,
+            WEIGHT        INT);
+            ''')
+except:
+    pass
+# insert query to insert food  details in
+# the above table
+connection.execute("INSERT INTO hotel VALUES (13, 'casdkes',800,10 )")
+connection.execute("INSERT INTO hotel VALUES (22, 'biscaasduits',100,20 )")
+connection.execute("INSERT INTO hotel VALUES (43, 'chocasdos',1000,30 )")
+connection.commit()
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="garshasp",
-  password="2411"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SHOW DATABASES")
-
-for x in mycursor:
-  print(x)
+print("All data in food table\n")
+ 
+# create a cousor object for select query
+cursor = connection.execute("SELECT * from hotel ")
+ 
+# display all data from hotel table
+for row in cursor:
+    print(row)
