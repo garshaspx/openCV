70e09659f95392fcc4cb6f094433b225f538c3b8
amirhosein24 <am.heidari24@gmail.com>
2023-08-15 13:35:01 +0330

    library import optimized

--- main.py
+++ main.py
@@ -9,78 +9,57 @@
 
 
 
-
-
-
-
-
-import sys
-import uuid
-import cv2
-import os
-import tkinter
-import sqlite3
-import threading
-from tkinter import filedialog
-from tkinter import ttk
-from tkinter import messagebox
+from sys import exit
+from uuid import uuid4
+from cv2 import VideoCapture, imshow, waitKey, destroyAllWindows, imwrite
+from os import path, mkdir, getcwd
+from tkinter import Tk, Label, Button, Entry, StringVar, PhotoImage, messagebox, filedialog, ttk
+from sqlite3 import connect
+from threading import Thread
 from ultralytics import YOLO
 from datetime import datetime
-from PIL import Image, ImageTk
-
-
-
-
-# from cv2 import VideoCapture, cvtColor, imread, convertScaleAbs, COLOR_BGR2GRAY, NORM_L1, BFMatcher, FONT_HERSHEY_PLAIN, imshow, waitKey, destroyAllWindows, xfeatures2d, putText
-# from os import getcwd, listdir, mkdir, path
-# from numpy import concatenate, loadtxt, float32
-# from tkinter import Tk, PhotoImage, Label, Button, Entry, StringVar
-# from tkinter.filedialog import askdirectory, askopenfilename
-# from tkinter.ttk import Treeview, Scrollbar, Combobox
-# from tkinter.messagebox import showerror
-# from getpass import getuser
-# from pandas import ExcelWriter, DataFrame
-# from datetime import datetime
-# from PIL import Image, ImageTk
+from PIL import ImageTk, Image
 
 
 
 
 
-home = os.getcwd() + "\\"
+
+
+home = getcwd() + "\\"
 try:
-    os.mkdir(home+"ML_train")
+    mkdir(home+"ML_train")
 except:
     pass
 try:
-    os.mkdir(home+"ML_train\\train")
+    mkdir(home+"ML_train\\train")
 except:
     pass
 try:   
-    os.mkdir(home+"ML_train\\train\\images")
+    mkdir(home+"ML_train\\train\\images")
 except:
     pass
 try:
-    os.mkdir(home+"ML_train\\train\\labels")
+    mkdir(home+"ML_train\\train\\labels")
 except:
     pass
 try:
-    os.mkdir(home+"ML_train\\valid")
+    mkdir(home+"ML_train\\valid")
 except:
     pass
 try:
-    os.mkdir(home+"ML_train\\valid\\images")
+    mkdir(home+"ML_train\\valid\\images")
 except:
     pass
 try:
-    os.mkdir(home+"ML_train\\valid\\labels")
+    mkdir(home+"ML_train\\valid\\labels")
 except:
     pass
 
 index = 0
 arr = []
 while True:
-    cap = cv2.VideoCapture(index)
+    cap = VideoCapture(index)
     if not cap.read()[0]:
         break
     else:
@@ -88,11 +67,11 @@ while True:
     cap.release()
     index += 1
 
-win = tkinter.Tk()
+win = Tk()
 win.title("image processor")
 win.geometry("400x290")
 win.resizable(width=False, height=False)
-win.iconphoto(False, tkinter.PhotoImage(file = home + '\\media\\icon.png'))
+win.iconphoto(False, PhotoImage(file = home + '\\media\\icon.png'))
 file_adress = home+"data.txt"
 
 def time():
@@ -111,7 +90,7 @@ except:
 info = ["None", 0, "ON"]
 def library():
     global info
-    lib_win = tkinter.Tk()
+    lib_win = Tk()
     lib_win.title("library manager")
     lib_win.geometry("410x260")
     lib_win.resizable(width=False, height=False)
@@ -135,16 +114,16 @@ def library():
     refresh_lib()
     
     def add_direc_txt():
-        new_library_win = tkinter.Tk()
+        new_library_win = Tk()
         new_library_win.title("new library")
         new_library_win.geometry("370x100")
         new_library_win.resizable(width=False, height=False)
-        tkinter.Label(new_library_win, text="enter information for new library :").grid(row=0, column=1)
-        tkinter.Label(new_library_win, text="enter name :").grid(row=1, column=0)
-        tkinter.Label(new_library_win, text="choose address :").grid(row=2, column=0)
-        name_entry = tkinter.Entry(new_library_win)
+        Label(new_library_win, text="enter information for new library :").grid(row=0, column=1)
+        Label(new_library_win, text="enter name :").grid(row=1, column=0)
+        Label(new_library_win, text="choose address :").grid(row=2, column=0)
+        name_entry = Entry(new_library_win)
         name_entry.grid(row=1, column=1)
-        tkinter.Button(new_library_win, text="choose:", command=lambda : choose_direc()).grid(row=2, column=1) 
+        Button(new_library_win, text="choose:", command=lambda : choose_direc()).grid(row=2, column=1) 
         address = "----"
     
         def choose_direc():
@@ -152,8 +131,8 @@ def library():
             file = filedialog.askopenfile(mode='r', filetypes=[('data Files', '*.pt')])
 
             if file:
-                address = os.path.abspath(file.name)
-            #tkinter.Label(new_library_win, text= f"chosen library is: {address}").grid(row=2, column=3)
+                address = path.abspath(file.name)
+            #Label(new_library_win, text= f"chosen library is: {address}").grid(row=2, column=3)
             win.bind('<FocusIn>', win.lower())
     
         def save():
@@ -166,7 +145,7 @@ def library():
             refresh_lib()
             new_library_win.destroy()
             win.bind('<FocusIn>', win.lower())
-        tkinter.Button(new_library_win, text="save new data", command= lambda : save()).grid(row=3, column=3)
+        Button(new_library_win, text="save new data", command= lambda : save()).grid(row=3, column=3)
     
     def delete():
         con_list = []
@@ -193,9 +172,9 @@ def library():
         kk = con_file.readlines()
         info[0] = kk[ac]
         lib_win.destroy()
-    tkinter.Button(lib_win, text="add library", command=lambda : add_direc_txt()).place(x=40, y=220)
-    tkinter.Button(lib_win, text="delete library", command=lambda: delete()).place(x=135, y=220)
-    tkinter.Button(lib_win, text="active library", command=lambda: active()).place(x=240, y=220)
+    Button(lib_win, text="add library", command=lambda : add_direc_txt()).place(x=40, y=220)
+    Button(lib_win, text="delete library", command=lambda: delete()).place(x=135, y=220)
+    Button(lib_win, text="active library", command=lambda: active()).place(x=240, y=220)
 
 
 
@@ -203,33 +182,34 @@ def start():
     global info
     if info[0] == "None":
         messagebox.showerror("library error", "choose your library before starting")
+    
         return    
     elif info[1] == False:
         messagebox.showerror("input error", "choose video input")
         return
-    win_start = tkinter.Tk()
+    win_start = Tk()
     win_start.title("image matcher")
     win_start.geometry("300x140")
     win_start.state("normal")
     win.resizable(width=False, height=False)
-    tkinter.Label(win_start, text="start matching :").place(x=10, y=20)
-    tkinter.Button(win_start, text="start", command=lambda: threading.Thread(target=start_match).start()).place(x=100, y=20)
-    tkinter.Label(win_start, text="stop matching :   press Esc on your keyboard").place(x=10, y=90)
+    Label(win_start, text="start matching :").place(x=10, y=20)
+    Button(win_start, text="start", command=lambda: Thread(target=start_match).start()).place(x=100, y=20)
+    Label(win_start, text="stop matching :   press Esc on your keyboard").place(x=10, y=90)
 
     def switch():
         if info[2] == "ON":
             info[2] = "OFF"
-            switch_but = tkinter.Button(win_start, text=info[2], command=lambda: switch())
+            switch_but = Button(win_start, text=info[2], command=lambda: switch())
             switch_but.place(x=250, y=45)
             win_start.update()
         else:
             info[2] = "ON"
-            switch_but = tkinter.Button(win_start, text=info[2]+" ", command=lambda: switch())
+            switch_but = Button(win_start, text=info[2]+" ", command=lambda: switch())
             switch_but.place(x=250, y=45)
             win_start.update()
-    switch_lab = tkinter.Label(win_start, text="view mode :")
+    switch_lab = Label(win_start, text="view mode :")
     switch_lab.place(x=170, y=45)
-    switch_but = tkinter.Button(win_start, text=info[2], command=lambda: switch())
+    switch_but = Button(win_start, text=info[2], command=lambda: switch())
     switch_but.place(x=250, y=45)
         
     adds = info[0].rstrip().split("==")
@@ -237,13 +217,13 @@ def start():
     threshold = 0.7  #add threshold option
 
     if info[1] == "0":
-        cap = cv2.VideoCapture(0)
+        cap = VideoCapture(0)
     else:
-        cap = cv2.VideoCapture(info[1])
+        cap = VideoCapture(info[1])
     
     def start_match():        
         global info
-        connection = sqlite3.connect(home+'data_center.db')
+        connection = connect(home+'data_center.db')
         
         try:
             connection.execute(''' CREATE TABLE \"data_center\"
@@ -256,7 +236,7 @@ def start():
         except:
             pass
         
-        uuid4 = str(uuid.uuid4())
+        uuid = str(uuid4())
         while True:
             _, frame = cap.read()
             view = frame
@@ -271,27 +251,28 @@ def start():
                 if conf >= threshold:
                     view = results[0].plot()
                     try:  
-                        connection.execute(f"INSERT INTO \"data_center\" values (\"{uuid4+str(box.id[0].item())}\", \"{class_id}\", {conf}, \"{cords}\", \"{time()}\")")
+                        connection.execute(f"INSERT INTO \"data_center\" values (\"{uuid+str(box.id[0].item())}\", \"{class_id}\", {conf}, \"{cords}\", \"{time()}\")")
                         connection.commit()
-                        cv2.imwrite(f"{home}ML_train\\train\\images\\{time()}_{class_id}.jpg", frame)
+                        imwrite(f"{home}ML_train\\train\\images\\{time()}_{class_id}.jpg", frame)
                         open(f"{home}ML_train\\train\\labels\\{time()}_{class_id}.txt", "w+").write(f"{int(id_item)} {((cords[0]+cords[2])/2/frame.shape[1])} {((cords[1]+cords[3])/2/frame.shape[0])} {(cords[2]-cords[0])/frame.shape[1]} {(cords[3]-cords[1])/frame.shape[0]}")#x center y center width hight
                     except:
                         pass
-            cv2.imshow("item Tracker", view)        
-            if cv2.waitKey(1) == 27 :
-                cv2.destroyAllWindows()
+            imshow("item Tracker", view)        
+            if waitKey(1) == 27 :
+                destroyAllWindows()
                 break
 
 
 
 def video():
-    vid_win = tkinter.Tk()
+    vid_win = Tk()
     vid_win.title("choose video input")
     vid_win.geometry("320x110")
     vid_win.resizable(width=False, height=False)
-    tkinter.Label(vid_win, text="choose a camera : ").place(x=20, y=10)
+    Label(vid_win, text="choose a camera : ").place(x=20, y=10)
     
-    cam_chooser = ttk.Combobox(vid_win, width = 20, textvariable = tkinter.StringVar())
+    cam_chooser = ttk.Combobox(vid_win, width = 20, textvariable = StringVar())
+    print(type(cam_chooser))
     if arr != []:
         cam_chooser['values'] = arr
         cam_chooser.current(0)
@@ -301,9 +282,9 @@ def video():
         cam_chooser.current(0)
     cam_chooser.place(x=150, y=10) 
         
-    tkinter.Label(vid_win, text="choose a video : ").place(x=20, y=50)
-    tkinter.Button(vid_win, text="choose", command=lambda:video_loc()).place(x=180, y=50)    
-    tkinter.Button(vid_win, text="save", fg="red", command=lambda:save()).place(x=270, y=50)
+    Label(vid_win, text="choose a video : ").place(x=20, y=50)
+    Button(vid_win, text="choose", command=lambda:video_loc()).place(x=180, y=50)    
+    Button(vid_win, text="save", fg="red", command=lambda:save()).place(x=270, y=50)
 
     def video_loc():
         info[1] = filedialog.askopenfile(filetypes=[('data Files', '*.mp4')]).name
@@ -320,12 +301,12 @@ def video():
 
 # add setting to choose camera and other stuff
 def setting():
-    s_win = tkinter.Tk()
+    s_win = Tk()
     s_win.title("setting")
     s_win.geometry("300x150") 
     s_win.resizable(width=False, height=False)
-    tkinter.Label(s_win, text="options will be coming soon", fg="red").pack()
-    connection = sqlite3.connect(home+'data_center.db')
+    Label(s_win, text="options will be coming soon", fg="red").pack()
+    connection = connect(home+'data_center.db')
     try:
         connection.execute(f''' CREATE TABLE setting
                 (pt TEXT PRIMARY KEY     NOT NULL,
@@ -337,22 +318,22 @@ def setting():
 
 
 def train():
-    train_win = tkinter.Tk()
+    train_win = Tk()
     train_win.title("train")
     train_win.geometry("300x120")
     # train_win.resizable(width=False, height=False)
-    tkinter.Label(train_win, text="choose algoritm to train model :").place(x=10, y=10)
-    tkinter.Button(train_win, text="choose:", command=lambda : choose_direc()).place(x=210, y=10)
-    tkinter.Button(train_win, text="start training", command= lambda : threading.Thread(target=start_train).start()).place(x=200, y=80)
-    tkinter.Button(train_win, text="stop training", bg="red" ,command= lambda : stop_tain()).place(x=100, y=80)
+    Label(train_win, text="choose algoritm to train model :").place(x=10, y=10)
+    Button(train_win, text="choose:", command=lambda : choose_direc()).place(x=210, y=10)
+    Button(train_win, text="start training", command= lambda : Thread(target=start_train).start()).place(x=200, y=80)
+    Button(train_win, text="stop training", bg="red" ,command= lambda : stop_tain()).place(x=100, y=80)
     
     address = ""
     def choose_direc():
         nonlocal address
         file_ad = filedialog.askopenfile(mode='r', filetypes=[('data Files', '*.pt')])
         if file_ad:
-            address = os.path.abspath(file_ad.name)
-            tkinter.Label(train_win, text= f"chosen:{address}", fg="red").place(x=10, y=35)
+            address = path.abspath(file_ad.name)
+            Label(train_win, text= f"chosen:{address}", fg="red").place(x=10, y=35)
         win.bind('<FocusIn>', win.lower())    
         
     def start_train():
@@ -370,7 +351,7 @@ def train():
 
 
 
-label = tkinter.Label(win)
+label = Label(win)
 label.place(x=-60, y=-30)
 image = Image.open(home+"\\media\\item_detec.gif")
 frames = []
@@ -387,12 +368,12 @@ update_frame(0)
 
 
 
-labe_intro = tkinter.Label(text="wellcome").place(x=190, y=10)
-tkinter.Button(win, text="start", command= lambda : start(), fg="blue").place(x=10, y=50)
-tkinter.Button(win, text="choose input", command=lambda: video()).place(x=10, y=85)
-tkinter.Button(win, text="library manager", command= lambda : library()).place(x=10, y=120)
-tkinter.Button(win, text="ML trainer", command= lambda : train()).place(x=10, y=155)
-tkinter.Button(win, text="setting", command= lambda : setting()).place(x=10, y=190)
-tkinter.Button(win, text="close", command= lambda : sys.exit("Error message"), fg="red").place(x=10, y=225)
+labe_intro = Label(text="wellcome").place(x=190, y=10)
+Button(win, text="start", command= lambda : start(), fg="blue").place(x=10, y=50)
+Button(win, text="choose input", command=lambda: video()).place(x=10, y=85)
+Button(win, text="library manager", command= lambda : library()).place(x=10, y=120)
+Button(win, text="ML trainer", command= lambda : train()).place(x=10, y=155)
+Button(win, text="setting", command= lambda : setting()).place(x=10, y=190)
+Button(win, text="close", command= lambda : exit("Error message"), fg="red").place(x=10, y=225)
 
 win.mainloop()
