1ce558a79a0329b7d99c2cc6b3dfc2e35412157c
amirhosein24 <138655921+amirhosein24@users.noreply.github.com>
2023-08-21 20:35:39 -0700

    shortcut left

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
-import subprocess
-import os
+from subprocess import CalledProcessError, call
+from os import chdir, path
 
+packages = {'opencv-python':'4.8.0.76', 'ultralytics':'8.0.154', 'pillow':'10.0.0', 'Nuitka':'1.7.10'}
+for package, version in packages.items():
+    try:
+        call(['pip', 'install', f"{package}=={version}"])
+        print(f"Successfully installed {package} version {version}")
+    except CalledProcessError:
+        print(f"Failed to install {package}")
 
-
-def install_packages(packages):
-    for package, version in packages.items():
-        try:
-            subprocess.call(['pip', 'install', f"{package}=={version}"])
-            print(f"Successfully installed {package} version {version}")
-        except subprocess.CalledProcessError:
-            print(f"Failed to install {package}")
-packages_to_install = {
-    'opencv-python':'4.8.0.76',
-    'ultralytics':'8.0.154',
-    'pillow':'10.0.0',
-    'Nuitka':'1.7.10'
-}
-
-install_packages(packages_to_install)
-
-
-current_file_path = os.path.abspath(__file__)
-current_directory = os.path.dirname(current_file_path)
-os.chdir(current_directory)
-
-
-
+current_file_path = path.abspath(__file__)
+current_directory = path.dirname(current_file_path)
+chdir(current_directory)
 
 
 print("installig ...")
-
-subprocess.call(f"python -m nuitka main_app.py")
-
+call(f"python -m nuitka --enable-plugin=tk-inter --disable-console --windows-icon-from-ico=media/icon.ico main_app.py")
 print("-------installination is done")
+
+
+
+
+
+# executable_path = "main_app.exe"
+# # Get the desktop directory
+# desktop_directory = path.join(path.expanduser("~"), "Desktop")
+# # Create a shortcut file name
+# shortcut_name = "item_detector.lnk"
+# # Create the command to create the shortcut
+# command = 'powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut(\'{0}\');$s.TargetPath=\'{1}\';$s.Save()"'.format(
+#     path.join(desktop_directory, shortcut_name),
+#     path.abspath(executable_path)
+# )
+# # Create the shortcut using subprocess
+# call(command, shell=True)
+
+
+
+
 input("-------press enter to exit ... ")
