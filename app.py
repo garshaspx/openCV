8812323d36ab890d3c44c34609dd296baf836df0
Amirhosein Heidari <138655921+amirhosein24@users.noreply.github.com>
2023-09-26 22:14:03 +0330

    update

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ def yol(): #fun to load ultralytics library
     except:
         pass
     win.update() #clearing label after loading
-
 yol_thread = Thread(target=yol)  #put it in a new thread
 yol_thread.start()  #starting thread
 
@@ -108,13 +107,11 @@ except:
     pass
 
 
-
 win.title("image processor") #title
 win.geometry("400x290")
 # win.resizable(width=False, height=False) #make its size unchangable
 win.iconphoto(False, PhotoImage(file = home + '/media/icon.png'))
 file_adress = home+"data.txt" #txt file to store data-set locations
-
 
 
 def time(): #function to retun time
@@ -126,7 +123,6 @@ try: #opening txt file of data-set locations
     open(file_adress).close()                           #
 except:                                                 # need to be updated to sql data base
     open(file_adress, "w+")                             #
-
 
 #info = [library-address,  video-input-index, view "ON or OFF", threshold, bg gif, image for video, machinelearning dataset]
 info = ["None"          ,  0                , "ON"            , 0.6      , "ON"  , "OFF"          , "OFF"]
@@ -158,12 +154,6 @@ except:
     x = connection.execute("SELECT * FROM setting") #if table does exist reads data
     for row in x:
         info = list(row)
-
-
-
-
-
-
 
 
 
@@ -209,7 +199,6 @@ def library(): #function to manage library manager window
         def choose_direc():
             nonlocal address
             file = filedialog.askopenfile(mode='r', filetypes=[('data Files', '*.pt')])
-
             if file:
                 address = path.abspath(file.name)
             #Label(new_library_win, text= f"chosen library is: {address}").grid(row=2, column=3)
@@ -277,7 +266,6 @@ def video():        #func to choose video input
     vid_win.geometry("320x110")
     # vid_win.resizable(width=False, height=False)
     Label(vid_win, text="choose a camera : ").place(x=20, y=10)
-
     cam_chooser = ttk.Combobox(vid_win, width = 20, textvariable = StringVar()) #creating a combobox
     if arr != []:  #adding cams to combobox
         cam_chooser['values'] = arr
@@ -364,15 +352,14 @@ def start():           #main func to start the program and start window
     switch_lab.place(x=170, y=20)
     switch_but = Button(win_start, text=info[2], command=lambda: switch())
     switch_but.place(x=260, y=20)
-    
     adds = info[0].rstrip().split("==") 
     model = YOLO(adds[1]) #loading data-set
+    # model.to('cuda')
 
     if info[1] == "0": #loading webcam
         cap = VideoCapture(0)
     else:
         cap = VideoCapture(info[1])
-    
     def start_match():        #starting the main prosec
         connection = connect(home+'data_center.db')#connecting to data base
         uuid = str(uuid4()) #create a uniqe id , its used in database
@@ -380,7 +367,7 @@ def start():           #main func to start the program and start window
         i, j = 0, 0
         while True: #main loop 
             _, frame = cap.read()
-            results = model.track(frame, persist=True, conf=info[3])#proccessing the frame              #  save_txt=True save data in txt             , device=[2]
+            results = model.track(frame, persist=True, conf=info[3])#proccessing the frame    , device="GPU"          #  save_txt=True save data in txt             , device=[2]
             result = results[0] 
             
             for box in result.boxes:         # puting bouding box around found items
@@ -389,7 +376,6 @@ def start():           #main func to start the program and start window
                 conf = round(box.conf[0].item(), 2)
                 id_item = box.cls[0].item()
                 # if conf >= info[3]:        #threshold if    test!!!!
- 
                 try:   # storing data in database and saving image and labels for training
                     connection.execute(f"INSERT INTO \"data_center\" values (\"{uuid+str(box.id[0].item())}\", \"{class_id}\", {conf}, \"{cords}\", \"{time()}\")")           
                     connection.commit()
@@ -407,9 +393,7 @@ def start():           #main func to start the program and start window
                         open(f"{home}ML_train/valid/labels/{time()}_{class_id}.txt", "w+").write(f"{int(id_item)} {((cords[0]+cords[2])/2/frame.shape[1])} {((cords[1]+cords[3])/2/frame.shape[0])} {(cords[2]-cords[0])/frame.shape[1]} {(cords[3]-cords[1])/frame.shape[0]}")#x center y center width hight
                         i = 0 
                     i += 1  
-
                 frame = results[0].plot()
-
 
             if info[5] == "ON":
                 numstr = str(j).zfill(9)
@@ -442,10 +426,7 @@ def train():    #creating tkinter window to train a new data-set
     train_win.title("train")
     train_win.geometry("300x120")
     # train_win.resizable(width=False, height=False)
-    Label(train_win, text="choose algif_playerritm to train model :").place(x=10, y=10)
-    Button(train_win, text="choose:", command=lambda : choose_direc()).place(x=210, y=10)
-    Button(train_win, text="start training", command= lambda : Thread(target=start_train).start()).place(x=200, y=80)
-    Button(train_win, text="stop training", bg="red" ,command= lambda : stop_tain()).place(x=100, y=80)
+
     address = ""
     def choose_direc():  #loading training algif_playerritm
         nonlocal address
@@ -454,6 +435,7 @@ def train():    #creating tkinter window to train a new data-set
             address = path.abspath(file_ad.name)
             Label(train_win, text= f"chosen:{address}", fg="red").place(x=10, y=35)
         win.bind('<FocusIn>', win.lower())
+
     def start_train(): 
         nonlocal address
         try:
@@ -461,22 +443,20 @@ def train():    #creating tkinter window to train a new data-set
             model.train(data=home + "ML_train/data.yaml", epochs=30)
         except:
             messagebox.showerror("training error", "make sure all data is correct")
+
+    Label(train_win, text="choose algif_playerritm to train model :").place(x=10, y=10)
+    Button(train_win, text="choose:", command=lambda : choose_direc()).place(x=210, y=10)
+    trainer = Thread(target=start_train)
+    Button(train_win, text="start training", command= lambda : trainer.start()).place(x=200, y=80)
+    Button(train_win, text="stop training", bg="red" ,command= lambda : stop_tain()).place(x=100, y=80)
+
+
+
     def stop_tain():
+        trainer.raise_exception(SystemExit)
         # add terminate option
         # x.terminate()
         return
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
